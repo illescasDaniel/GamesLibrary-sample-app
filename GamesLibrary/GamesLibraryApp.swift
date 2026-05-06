cf211732da3f2a6b555917e310b0f1c1446fd9da
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2026-05-06 11:51:40 +0200

    Initial Commit

---
+++ GamesLibrary/GamesLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesLibraryApp.swift
+//  GamesLibrary
+//
+//  Created by Daniel Illescas Romero on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GamesLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GamesLibraryTests/GamesLibraryTests.swift
@@ -0,0 +1,19 @@
+//
+//  GamesLibraryTests.swift
+//  GamesLibraryTests
+//
+//  Created by Daniel Illescas Romero on 6/5/26.
+//
+
+import Testing
+@testable import GamesLibrary
+
+struct GamesLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
